7e6d65851b599adee16bb525145a38fa4dc4f242
Gabriel Henrique <turles41@gmail.com>
2023-03-20 17:40:54 -0300

    Adicionar arquivos de projeto.

---
+++ 1 - somaDeElementos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int A, B, SOMA;
+
+        Console.Write("Digite o valor de A: ");
+        A = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de B: ");
+        B = int.Parse(Console.ReadLine());
+
+        SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+    }
+}
---
+++ 2 - mediaPonderada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double A, B, C, MEDIA;
+
+        Console.WriteLine("Digite a média da primeira matéria: ");
+        A = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a média da segunda matéria: ");
+        B = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a média da terceira matéria: ");
+        C = double.Parse(Console.ReadLine());
+
+        MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 100;
+
+        Console.WriteLine("MEDIA = " + MEDIA);
+    }
+}
